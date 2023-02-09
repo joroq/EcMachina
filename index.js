a38ff2f5e684e307ec5aedeb68487d3a2f1771b5
Joanne Greenhalgh <joanne.greenhalgh@roq.co.uk>
2023-02-09 10:07:59 +0000

    Simple console To-do list

---
+++ index.js
@@ -0,0 +1,44 @@
+const prompt = require('prompt-sync')();
+
+console.log('To-Do List');
+
+let command = '';
+let response = '';
+
+let allowedCommands = {
+    add : 'Adds an item to the list',
+    show : 'Shows all tasks',
+    remove : 'Choose an item to remove from the list',
+    quit : 'Quit the program'
+};
+let tasks = [];
+
+while (command.toUpperCase() != 'QUIT') {
+    response = prompt('Enter command (quit to exit): ');
+    command = response.toUpperCase();
+
+    if (command == 'ADD') {
+        let task = prompt('Enter task to add: ');
+        tasks.push(task);
+        console.log(tasks);
+    }
+    else if (command == 'SHOW') {
+        console.log(tasks);
+    }
+    else if (command == 'REMOVE') {
+        console.log('Enter "Y" to remove task shown');
+        for (let task in tasks) {
+            console.log(tasks[task]);
+            let select = prompt('Remove ' + tasks[task] + '? ');
+            if (select.toUpperCase() == 'Y') {
+                tasks.splice(task);
+                console.log(tasks);
+            }
+        }
+    }
+    else if (command == 'HELP') {
+        for (let allowed in allowedCommands) {
+            console.log(allowed + ': ' + allowedCommands[allowed]);
+        }
+    }
+}
